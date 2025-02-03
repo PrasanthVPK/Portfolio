63748514d8b86fbfbdb8aa84a25e4be2282ef21f
PrasanthVPK <prasanthkumarv02@gmail.com>
2025-02-03 20:56:40 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PortfolioApp from './component/PortfolioApp.tsx';
+
+function App() {
+  return (
+    <div>
+      <PortfolioApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/PortfolioApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar.tsx'
+
+const PortfolioApp = () => {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default PortfolioApp
