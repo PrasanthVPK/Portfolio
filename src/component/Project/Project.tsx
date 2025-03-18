b79a938e10b4c4b8e06224ee21d3e9c0fb6c0396
PrasanthVPK <prasanthkumarv02@gmail.com>
2025-03-18 16:02:18 +0530

    animation changes

--- src/component/Project/Project.tsx
+++ src/component/Project/Project.tsx
@@ -1,35 +1,31 @@
-  import React, { useState } from "react";
-  import { project_experience } from "../mock";
-  import "./project.css";
+import { project_experience } from "../mock";
+import "./project.css";
 
-  const Project = () => {
-    const [isSelectedProject, setSelectedProject] = useState<string | null>("");
+const Project = () => {
+  return (
+    <>
+      <h1 className="mb-5 project-heading text-center">Project Experience</h1>
 
-    const handleDetails = (name: string) => {
-      setSelectedProject(name);
-    };
-
-    return (
-      <>
-        <h1 className="mb-5 project-heading text-center">Project Experience</h1>
-
-        <div className="d-lg-flex">
-          {project_experience.map((values, index) => (
-            <div key={index} className="col-lg-6 p-4 d-lg-flex bg-white mx-2 project-card">
-              <div className="d-flex align-items-center justify-content-center mx-lg-4 m-0">
-                {values.img}
-              </div>
-
-              <div>
-                <h4>{values.project_name}</h4>
-                <h5>{values.company_name}</h5>
-                <div>{values.desc1}</div>
-              </div>
+      <div className="d-lg-flex" data-aos="fade-in">
+        {project_experience.map((values, index) => (
+          <div
+            key={index}
+            className="col-lg-6 p-4 d-lg-flex bg-white mx-2 project-card"
+          >
+            <div className="d-flex align-items-center justify-content-center mx-lg-4 m-0">
+              {values.img}
             </div>
-          ))}
-        </div>
-      </>
-    );
-  };
 
-  export default Project;
+            <div>
+              <h4>{values.project_name}</h4>
+              <h5>{values.company_name}</h5>
+              <div>{values.desc1}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Project;
