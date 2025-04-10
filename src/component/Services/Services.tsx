8ff43aeebdcb50303e0cb7b8ddbddc41d20bdaa5
PrasanthVPK <prasanthkumarv02@gmail.com>
2025-04-10 13:21:24 +0530

    Service Changes

--- src/component/Services/Services.tsx
+++ src/component/Services/Services.tsx
@@ -1,27 +1,100 @@
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import { service_info } from "../mock";
 import "./service.css";
 
 const Services = () => {
+  const [chunkSize, setChunkSize] = useState(3);
+  const [groupedServices, setGroupedServices] = useState<any[][]>([]);
+
+  const getChunkSize = (width: number) => {
+    if (width < 576) return 1;
+    if (width < 992) return 2;
+    return 3;
+  };
+
+  const chunkArray = (arr: any[], size: number) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+      arr.slice(i * size, i * size + size)
+    );
+
+  const handleResize = () => {
+    const newSize = getChunkSize(window.innerWidth);
+    setChunkSize(newSize);
+    setGroupedServices(chunkArray(service_info, newSize));
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <>
       <h2 className="mb-5 text-center service-heading">Services & Expertise</h2>
 
-      <div className="d-flex justify-content-lg-evenly justify-content-center flex-wrap">
-        {service_info.map((values, index) => (
-          <div key={index} className="card swiper-slide col-lg-5 col-xl-3 col-md-9 col-sm-8 m-4">
-            <div className="image-content">
-              <span className="overlay"></span>
-              <div className="card-image d-flex align-items-center justify-content-center">
-                {values.service_icon}
+      <div
+        id="carouselControls"
+        className="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {groupedServices.map((group, idx) => (
+            <div
+              key={idx}
+              className={`carousel-item ${idx === 0 ? "active" : ""}`}
+            >
+              <div className="d-flex justify-content-evenly">
+                {group.map((service, sIdx) => (
+                  <div
+                    key={sIdx}
+                    className="card col-12 col-sm-10 col-md-6 col-lg-4 mx-3"
+                  >
+                    <div className="image-content">
+                      <span className="overlay"></span>
+                      <div className="card-image d-flex align-items-center justify-content-center">
+                        {service.service_icon}
+                      </div>
+                    </div>
+
+                    <div className="card-content">
+                      <h5 className="card-title text-center">
+                        {service.service_name}
+                      </h5>
+                      <p className="card-text text-center">
+                        {service.service_desc}
+                      </p>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
+          ))}
+        </div>
 
-            <div className="card-content">
-              <h5 className="card-title text-center">{values.service_name}</h5>
-              <p className="card-text text-center">{values.service_desc}</p>
-            </div>
-          </div>
-        ))}
+        <div className="d-flex justify-content-center mt-4">
+          <button
+            className="btn btn-primary mx-2 align-items-center d-flex"
+            type="button"
+            data-bs-target="#carouselControls"
+            data-bs-slide="prev"
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <button
+            className="btn btn-primary mx-2 align-items-center d-flex"
+            type="button"
+            data-bs-target="#carouselControls"
+            data-bs-slide="next"
+          >
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
       </div>
     </>
   );
